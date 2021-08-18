f1c051bce60d4517bcb25ac3411ec494376e540a
Aethor <oow.autre@gmail.com>
2021-08-18 14:52:07 +0200

    cleaning

--- impls/cpp.2/step0_repl.cpp
+++ impls/cpp.2/step0_repl.cpp
@@ -1,25 +1,25 @@
 #include <string>
 #include <iostream>
 
-std::string& read(std::string& input) {
+std::string read() {
+    std::string input;
+    std::getline(std::cin, input);
     return input;
 }
 
-std::string& eval(std::string& input) {
+std::string eval(std::string input) {
     return input;
 }
 
-std::string& print(std::string& input) {
-    return input;
+void print(std::string input) {
+    std::cout << input << "\n";
 }
 
 int main(int argc,char* argv[]) {
     while (true) {
 	std::string input;
 	std::cout << "user> ";
-	//std::cin >> input;
-	std::getline(std::cin, input);
-	std::cout << read(eval(print(input))) << "\n";
+	print(eval(read()));
     }
     return 0;
 }
